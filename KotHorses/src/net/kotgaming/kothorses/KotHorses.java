b444cbf6ebf1371a34dc889179b1803b44e194a5
DasLeo <wuddja@gmail.com>
2013-07-19 15:39:26 +0200

    New Project KotHorses added
    Signed-off-by: DasLeo <wuddja@gmail.com>

---
+++ KotHorses/src/net/kotgaming/kothorses/KotHorses.java
@@ -0,0 +1,16 @@
+package net.kotgaming.kothorses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KotHorses extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
