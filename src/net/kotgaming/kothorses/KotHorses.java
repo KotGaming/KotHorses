e93aa0945a450256d6e000a0a0f1a212009cb8cf
naddsch <naddsch@googlemail.com>
2013-07-19 19:27:54 +0200

    naddsch branch

--- src/net/kotgaming/kothorses/KotHorses.java
+++ src/net/kotgaming/kothorses/KotHorses.java
@@ -2,15 +2,17 @@ package net.kotgaming.kothorses;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class KotHorses extends JavaPlugin {
+public final class KotHorses extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		// TODO Insert logic to be performed when the plugin is enabled
+	}
 
-    @Override
-    public void onEnable(){
-        // TODO Insert logic to be performed when the plugin is enabled
-    }
- 
-    @Override
-    public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
-    }
+	@Override
+	public void onDisable()
+	{
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
 }
