0e87548099c66648643566349ae08f7fb0fbdeb2
naddsch <naddsch@googlemail.com>
2013-07-19 21:04:24 +0200

    generating config file

---
+++ src/net/kotgaming/kothorses/KotHorsesEventHandler.java
@@ -0,0 +1,15 @@
+package net.kotgaming.kothorses;
+
+import org.bukkit.event.Listener;
+
+public class KotHorsesEventHandler implements Listener
+{
+	@SuppressWarnings("unused")
+	private KotHorses parent;
+
+	public KotHorsesEventHandler(KotHorses kotHorses)
+	{
+		parent = kotHorses;
+	}
+
+}
